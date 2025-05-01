f6be54d94973a8c3e49c6671d9684719e8b73f8f
TingxuanLin <112202338+TingxuanLin@users.noreply.github.com>
2025-05-01 16:11:25 -0700

    Create env-config.js

---
+++ blog/client/public/env-config.js
@@ -0,0 +1,4 @@
+window.REACT_APP_COMMENTS_SERVICE_URL = "http://comments";
+window.REACT_APP_QUERY_SERVICE_URL = "http://query";
+window.REACT_APP_POSTS_SERVICE_URL = "http://posts";
+window.REACT_APP_EVENTBUS_SERVICE_URL = "http://event-bus";

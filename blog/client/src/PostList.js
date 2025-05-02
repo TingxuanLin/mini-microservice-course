81b198089bd4655ec5a568cc54ca1b593cfc71f6
TingxuanLin <112202338+TingxuanLin@users.noreply.github.com>
2025-05-01 17:13:37 -0700

    Update PostList.js

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -18,21 +18,21 @@ export default () => {
     fetchPosts();
   }, [posts]);
 
-  const renderedPosts = Object.values(posts).map((post) => {
-    return (
-      <div
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
-        key={post.id}
-      >
-        <div className="card-body">
-          <h3>{post.title}</h3>
-          <CommentList comments={post.comments || []} />
-          <CommentCreate postId={post.id} />
+  const renderedPosts = posts && Object.keys(posts).length > 0
+    ? Object.values(posts).map((post) => (
+        <div
+          className="card"
+          style={{ width: "30%", marginBottom: "20px" }}
+          key={post.id}
+        >
+          <div className="card-body">
+            <h3>{post.title}</h3>
+            <CommentList comments={post.comments || []} />
+            <CommentCreate postId={post.id} />
+          </div>
         </div>
-      </div>
-    );
-  });
+      ))
+    : <p>Loading posts...</p>;
 
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">

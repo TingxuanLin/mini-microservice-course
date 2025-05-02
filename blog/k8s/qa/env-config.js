f72c695f6467c2c4e8686e9465c7d8c1bdecfba9
TingxuanLin <112202338+TingxuanLin@users.noreply.github.com>
2025-05-01 17:20:18 -0700

    Update env-config.js

--- blog/k8s/qa/env-config.js
+++ blog/k8s/qa/env-config.js
@@ -1,5 +1,4 @@
-// env-config.js
-window.REACT_APP_COMMENTS_SERVICE_URL = "http://comments-svc.qa.svc.cluster.local:4001";
-window.REACT_APP_QUERY_SERVICE_URL = "http://query-svc.qa.svc.cluster.local:4002";
-window.REACT_APP_POSTS_SERVICE_URL = "http://posts-svc.qa.svc.cluster.local:4000";
-window.REACT_APP_EVENTBUS_SERVICE_URL = "http://event-bus-svc.qa.svc.cluster.local:4005";
+window.REACT_APP_COMMENTS_SERVICE_URL = "/comments";
+window.REACT_APP_QUERY_SERVICE_URL = "/query";
+window.REACT_APP_POSTS_SERVICE_URL = "/posts";
+window.REACT_APP_EVENTBUS_SERVICE_URL = "/event-bus";

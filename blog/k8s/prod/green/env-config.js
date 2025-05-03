b30930e1e8d718200664bff6f39649f04db2a0f4
TingxuanLin <tlin52@dons.usfca.edu>
2025-05-03 13:37:27 -0700

    adding prod stage and update qa and uat stage

---
+++ blog/k8s/prod/green/env-config.js
@@ -0,0 +1,4 @@
+window.REACT_APP_COMMENTS_SERVICE_URL = "/comments";
+window.REACT_APP_QUERY_SERVICE_URL = "/query";
+window.REACT_APP_POSTS_SERVICE_URL = "/posts";
+window.REACT_APP_EVENTBUS_SERVICE_URL = "/event-bus";
